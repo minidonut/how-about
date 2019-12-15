a3dc1017422e385d9a39cc2b5bc98742d7380bbd
Karl Saehun Chung <nycom13@gmail.com>
2019-12-15 21:09:25 +0900

    Scaffold project structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("how-about");
+import * as program from "commander";
+import main from "./main";
+const version = require("../package.json").version;
+
+program.version(version, "-v, --version");
+
+program
+  .arguments("<target> [type]")
+  .option("-v, --version", "Copy sprint key to system clipboard")
+  .option("-b, --describe", "Checkout git branch of issue-key at current directory")
+  .description("Add a issue into current sprint")
+  .action(main);
+
+program.parse(process.argv);
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const main = (...args: any) => {
+  console.log(args);
+};
+
+export default main;
