2dce85b82a2eae1fb486ff95157defa45bf41082
Karl Saehun Chung <nycom13@gmail.com>
2019-12-16 12:31:16 +0900

    Add checkers

--- src/modules/Context.ts
+++ src/modules/Context.ts
@@ -1,4 +1,13 @@
 import * as  path from "path";
+const dirTree = require("directory-tree");
+
+interface DirTree {
+  path: string;
+  name: string;
+  size: number;
+  type: "directory" | "file";
+  children: DirTree[];
+}
 
 export class Context {
   type: string;
@@ -7,14 +16,58 @@ export class Context {
   version: string;
   cwd: string;
   root: string;
+  tree: any;
 
   constructor(type: string, target: string, describe: boolean, version: string) {
-    console.log("node_env:: ", process.env.NODE_ENV);
     this.type = type;
     this.target = target;
     this.describe = describe;
     this.version = version;
     this.cwd = process.cwd();
+  }
+
+  load() {
+    const root = path.join(__dirname, ...Array(process.env.NODE_ENV === "production" ? 1 : 2).fill(".."));
+    const tree = dirTree(path.join(root, "data")) as DirTree;
+
+    // Check type
+    const typeTree = tree.children
+      .filter(x => x.type === "directory" && x.name[0] !== "_")
+      .find(x => x.name === this.type);
+    if (!typeTree) {
+      console.error(`invalid <type> arguments provided.
+choose one of\n
+${tree.children.filter(x => x.type === "directory").map(x => "- " + x.name).join("\n")}\n
+given: ${this.type}\n`);
+      process.exit(1);
+    }
+
+    // Check target
+    const targetTree = typeTree.children
+      .filter(x => x.type === "directory" && x.name[0] !== "_")
+      .find(x => x.name === this.target);
+    if (!targetTree) {
+      console.error(`invalid <target> arguments provided.
+choose one of\n
+${typeTree.children.filter(x => x.type === "directory").map(x => "- " + x.name).join("\n")}\n
+given: ${this.target}\n`);
+      process.exit(1);
+    }
+
+    // Check version
+    const versionTree = targetTree.children
+      .filter(x => x.type === "directory" && x.name[0] !== "_")
+      .find(x => x.name === this.version);
+    if (!versionTree && this.version !== "default") {
+      console.error(`invalid [version] optional arguments provided.
+choose one of\n
+${targetTree.children.filter(x => x.type === "directory").map(x => "- " + x.name).join("\n")}\n
+given: ${this.version}\n`);
+      process.exit(1);
+    }
+
+
+    return this;
   }
 
 }
--- src/modules/command.ts
+++ src/modules/command.ts
@@ -1,17 +1,6 @@
 import { describeVersion } from "./describe";
 import { Context } from "../modules/Context";
 
-const checkType = ({ type }: Context) => {
-  if (!["append", "typescript"].includes(type)) {
-    console.error("invalid <type> arguments provided. should be 'append' or 'typescript'");
-    console.error(`given: ${type}`);
-    process.exit(1);
-  }
-};
-
-const checkTarget = ({ type, target }: Context) => {
-  return;
-};
 
 export const command = (type: string, target: string, { describe, version }: any) => {
   const ctx = new Context(
@@ -20,8 +9,7 @@ export const command = (type: string, target: string, { describe, version }: any
     !!describe,
     typeof version === "string" ? version : "default",
   );
-  checkType(ctx);
-  checkTarget(ctx);
+  ctx.load();
 
   if (describe) {
     describeVersion(ctx);
