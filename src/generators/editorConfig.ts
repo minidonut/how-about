3b8292c889c5307ba961f51cece85e4083ebbdc8
Karl Saehun Chung <nycom13@gmail.com>
2019-12-18 06:37:17 +0900

    Make editorconfig works

--- src/generators/editorConfig.ts
+++ src/generators/editorConfig.ts
@@ -1,7 +1,25 @@
 import { GenFn } from "./types";
+import * as path from "path";
+import { popd, pushd } from "../modules/pushd";
+import {
+  findFileFromCustomTree,
+  findFileFromTargetTree,
+} from "../utils/findFile";
 
-const v1: GenFn = (ctx, ...args) => {
-  console.log("generate .edtiorconfig");
+const v1: GenFn = (ctx, varient) => {
+  const { projectPath, root, tree } = ctx.env;
+  pushd(projectPath);
+
+  // Search .editorconfig inside targetPath first
+  // than recursively try to find the file from ctx.env.root/append/editorconfig/*
+  // if command type is 'append', get selected version
+  // if not, choose default version of editorconfig respectively
+  const filePath = findFileFromTargetTree(".editorconfig", ctx, true) ||
+    findFileFromCustomTree(".editorconfig", path.join(root, "data", "append", "editorconfig"), varient);
+
+  console.log(filePath);
+
+  popd();
 };
 
 export default {
