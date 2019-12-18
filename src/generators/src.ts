f2608fb2072e590733958806f5e7ddea5eb75d68
Karl Saehun Chung <nycom13@gmail.com>
2019-12-18 17:02:40 +0900

    Add src folder generator

--- src/generators/src.ts
+++ src/generators/src.ts
@@ -1,7 +1,20 @@
 import { GenFn } from "./types";
+import * as path from "path";
+import {
+  findFileFromTargetTree,
+} from "../utils/findFile";
+import { logGenerator } from "../modules/logger";
+const copyDir = require("copy-dir");
 
-const v1: GenFn = (ctx, ...args) => {
-  console.log("generate src folder");
+const v1: GenFn = (ctx) => {
+  const { projectPath } = ctx.env;
+  const logDone = logGenerator("/src/**/*");
+
+  const copyPath = findFileFromTargetTree("src", ctx, true)?.path;
+  const pastePath = path.join(projectPath, "src");
+
+  copyDir.sync(copyPath, pastePath);
+  logDone();
 };
 
 export default {
